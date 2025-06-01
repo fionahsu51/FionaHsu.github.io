ef88963bf8af00ffbc289c3f28d21755e3e9040b
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2025-05-31 18:28:03 -0700

    updated domain name again

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fionahsu.com',
+  site: 'https://fionahsu51.github.io',
   integrations: [mdx(), svelte(), react(), preact()],
   markdown: {
     shikiConfig: {
